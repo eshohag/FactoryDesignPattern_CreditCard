318d05e1def9e2adadfbdac5b02e4bba7569605a
MD Shohag Mia <eshohag@outlook.com>
2023-10-30 21:33:59 +0600

    Add project files.

---
+++ FactoryDesignPattern/Cards/GoldCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern.Cards
+{
+    public class GoldCard : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Gold Card";
+        }
+        public int GetCreditLimit()
+        {
+            return 150000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 1000;
+        }
+    }
+}
---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using FactoryDesignPattern.Cards;
+
+namespace FactoryDesignPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreditCard cardDetails = new GoldCard();
+            Console.WriteLine(cardDetails.GetCardType());
+
+            cardDetails = new PlatinumCard();
+            Console.WriteLine(cardDetails.GetCardType()); 
+
+            cardDetails = new TitaniumCard();
+            Console.WriteLine(cardDetails.GetCardType());
+
+            Console.ReadKey();
+        }
+    }
+}
